3eeeaae9552374a641b467175867fa020fcd84e7
mesodiar <mils@prontomarketing.com>
2020-05-20 14:16:14 +0700

    :art: Create the function component :koala:

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-function tick(){
-  const element = (
+function Clock(props) {
+  return (
     <div>
       <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
     </div>
-  )
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
   );
 }
-setInterval(tick, 1000)
+function tick() {
+  ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"));
+}
+setInterval(tick, 1000);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
