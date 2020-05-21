39eef5dae22fc62baeaa975f5570d1cbbb132360
mesodiar <mils@prontomarketing.com>
2020-05-21 12:05:43 +0700

    :art: Add condition to Toggle event :koala:

--- src/toggle.js
+++ src/toggle.js
@@ -1,24 +1,41 @@
 import React, { Component } from "react";
 
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+
+  return <div className="warning">Warning</div>;
+}
+
 export default class Toggle extends Component {
   constructor(props) {
     super(props);
-    this.state = { isToggleOn: true };
+    this.state = { showWarning: true };
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
     this.setState((state) => ({
-      isToggleOn: !state.isToggleOn,
+      showWarning: !state.showWarning,
     }));
   }
 
+  // If we don't want to use `this.handleClick = this.handleClick.bind(this);`
+  // 1. Using public class field syntax
+  // handleClick = () => {
+  // }
+  // 2. Or use  <button onClick={() => this.handleClick()}>
+
   render() {
     return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? "ON" : "OFF"}
-      </button>
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleClick}>
+          {this.state.showWarning ? "HIDE" : "SHOW"}
+        </button>
+      </div>
     );
   }
 }
