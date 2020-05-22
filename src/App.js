e1caedff642bcfc692142648c238f028e5b6b3ff
mesodiar <mils@prontomarketing.com>
2020-05-22 10:15:38 +0700

    :sparkles: Add form :koala:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Clock from "./Clock";
 import Toggle from "./Toggle";
+import NameForm from "./NameForm";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Clock />
       <Toggle />
+      <NameForm />
     </div>
   );
 }
